931b502b207128fb1ba1fac57ac0557399c37d36
Bilel Raddadi <bilel.raddadi@irt-saintexupery.com>
2024-02-06 10:21:29 +0100

    update loop read lib

--- lib/loopProg/src/main_loopRead.cpp
+++ lib/loopProg/src/main_loopRead.cpp
@@ -6,18 +6,14 @@
 #include <errno.h>
 #include <stdbool.h>
 #include <unistd.h>
+#include <cstdio>
+#include <stdint.h>
 
 
 #define PROGRAM_VERSION "0.0.1"
 
 #define BUFF_SIZE 2048
 	
-enum FMT_DATA_TYPE {
-	FMT_U8 = 0,
-	FMT_S16,
-	FMT_FLOAT
-};
-
 typedef struct {
 	uint8_t re;  
 	uint8_t im;
@@ -27,13 +23,21 @@ typedef struct {
 typedef struct {
 	int16_t re;
 	int16_t im;
-}s16cmplx;
+}i16cmplx;
 
 
 typedef struct{
-	float re;
-	float im;
+    float re;
+    float im;
 }fcmplx;
+
+
+
+enum FMT_DATA_TYPE {
+	FMT_U8 = 0,
+	FMT_I16,
+	FMT_FLOAT
+};
 
 FILE *input_fid;
 FILE *output_fid;
@@ -59,9 +63,11 @@ void print_usage(){
 
 	fprintf(stderr,\
 	"loopRead -%s\n\
-	Usage:loopRead -i File [-t IQType][-h] \n\
+	Usage:loopRead -i File -o File [-t IQType][-h] \n\
 	-i            Input IQ File I16 format (default stdin) \n\
-	-t            IQType {float,u8,i16} (default float) \n\
+	-o            Output IQ File FLOAT format (default stdout) \n\
+	-r            IQType {float,u8,i16} (default float) \n\
+	-w            IQType {float,u8,i16} (default float) \n\
 	-h            help (print this help).\n\
 	Example : .\\loopRead -i - \n\
 	\n", \
@@ -76,12 +82,13 @@ int main(int argc, char *argv[]) {
 	input_fid  = stdin;
 	output_fid = stdout;
 	int dataTypeIn = FMT_FLOAT;
+	int dataTypeOut = FMT_FLOAT;
 	
 	int a;
 	bool anyargs = false;
 	while(1){
 		
-		a = getopt(argc, argv, "i:t:h");
+		a = getopt(argc, argv, "i:o:r:w:h");
 		if(a==-1){
 			if(anyargs){ break; }
 			else       { a = 'h'; } //print usage and exit
@@ -98,10 +105,24 @@ int main(int argc, char *argv[]) {
 					}
 				}
 				}break;
-			case 't':{ // Input Type
+			case 'o':{ // OutputFile
+					if(optarg != nullptr){
+						output_fid = fopen(optarg, "wb");
+						if( output_fid == nullptr ){
+							fprintf(stderr, "[LOOP RD] Unable to open '%s': %s\n",optarg, strerror(errno));
+							exit(EXIT_FAILURE);
+						}
+					}
+				}break;
+			case 'r':{ // Input Type
 					if (strcmp("float", optarg) == 0){ dataTypeIn = FMT_FLOAT; }
-					if (strcmp("s16", optarg) == 0)  { dataTypeIn = FMT_S16;   }
+					if (strcmp("i16", optarg) == 0)  { dataTypeIn = FMT_I16;   }
 					if (strcmp("u8", optarg) == 0)   { dataTypeIn = FMT_U8;    }
+				}break;
+			case 'w':{ // Output Type
+					if (strcmp("float", optarg) == 0){ dataTypeOut = FMT_FLOAT; }
+					if (strcmp("i16", optarg) == 0)  { dataTypeOut = FMT_I16;   }
+					if (strcmp("u8", optarg) == 0)   { dataTypeOut = FMT_U8;    }
 				}break;
 			case 'h':{ // help
 				print_usage();
@@ -139,12 +160,15 @@ int main(int argc, char *argv[]) {
 	// =======================================================================================================================================================================================================
 	// ########################################################################################################################################################################################################
 	// =======================================================================================================================================================================================================
-	fcmplx BufferIQ[BUFF_SIZE];    //buffer to hold complex values
+	fcmplx fBufferIQ[BUFF_SIZE];    //buffer to hold complex values
+	i16cmplx iBufferIQ[BUFF_SIZE];    //buffer to hold complex values
+	u8cmplx uBufferIQ[BUFF_SIZE];    //buffer to hold complex values
+	
 	bool repeat_reading = true;
 	unsigned long long FrameCount = 0;
 	m_keep_running = true;
 
-	while(m_keep_running = true;){
+	while(m_keep_running){
 		int nRead=0; 
 		switch(dataTypeIn){
 			case FMT_U8:{
@@ -152,17 +176,29 @@ int main(int argc, char *argv[]) {
 				nRead=fread(iqin_u8,sizeof(u8cmplx),BUFF_SIZE,input_fid);
 				if(nRead<=0){ break; } 
 				for(int i=0; i<nRead; i++){
-					BufferIQ[i].re = (iqin_u8[i].re - 127.5)/127.0;
-					BufferIQ[i].im = (iqin_u8[i].im - 127.5)/127.0;						
+					uBufferIQ[i].re = iqin_u8[i].re;
+					uBufferIQ[i].im = iqin_u8[i].im;
+					
+					fBufferIQ[i].re = (iqin_u8[i].re - 127.5)/127.0;
+					fBufferIQ[i].im = (iqin_u8[i].im - 127.5)/127.0;
+					
+					iBufferIQ[i].re = (int16_t)(32767.0*fBufferIQ[i].re);
+					iBufferIQ[i].im = (int16_t)(32767.0*fBufferIQ[i].im);						
 				}   
 			}break;
-			case FMT_S16:{
-				i16cmplx iqin_s16[BUFF_SIZE];
-				nRead = fread(iqin_s16, sizeof(i16cmplx),BUFF_SIZE,input_fid);           
+			case FMT_I16:{
+				i16cmplx iqin_i16[BUFF_SIZE];
+				nRead = fread(iqin_i16, sizeof(i16cmplx),BUFF_SIZE,input_fid);           
 				if(nRead<=0){ break; } 
 				for(int i=0; i<nRead; i++){
-					BufferIQ[i].re = iqin_i16[i].re/32767.0;
-					BufferIQ[i].im = iqin_i16[i].im/32767.0;						
+					iBufferIQ[i].re = iqin_i16[i].re;
+					iBufferIQ[i].im = iqin_i16[i].im;
+					
+					fBufferIQ[i].re = iqin_i16[i].re/32767.0;
+					fBufferIQ[i].im = iqin_i16[i].im/32767.0;
+					
+					uBufferIQ[i].re = (uint8_t)(128*fBufferIQ[i].re + 127);
+					uBufferIQ[i].im = (uint8_t)(128*fBufferIQ[i].im + 127);					
 				}
 			}break;
 			case FMT_FLOAT:{
@@ -170,8 +206,14 @@ int main(int argc, char *argv[]) {
 				nRead=fread(iqin_f32,sizeof(fcmplx),BUFF_SIZE,input_fid); 
 				if(nRead<=0){ break; }
 				for(int i=0; i<nRead; i++){
-					BufferIQ[i].re = iqin_f32[i].re;
-					BufferIQ[i].im = iqin_f32[i].im;               
+					uBufferIQ[i].re = (uint8_t)(128*iqin_f32[i].re + 127);
+					uBufferIQ[i].im = (uint8_t)(128*iqin_f32[i].im + 127);
+					
+					iBufferIQ[i].re = (int16_t)(32767.0*iqin_f32[i].re);
+					iBufferIQ[i].im = (int16_t)(32767.0*iqin_f32[i].im);
+					
+					fBufferIQ[i].re = iqin_f32[i].re;
+					fBufferIQ[i].im = iqin_f32[i].im;               
 				}    
 			}break;
 		}
@@ -182,9 +224,21 @@ int main(int argc, char *argv[]) {
 		}
 		else{
 			if(nRead<BUFF_SIZE){ fprintf(stderr, "[LOOP RD] Incomplete buffer %d/%d\n",nRead,BUFF_SIZE); }
-		
+		   
 			// Write on file
-			nWrite = fwrite(BufferIQ,sizeof(fcmplx),nRead,output_fid);
+			int nWrite;
+			switch(dataTypeOut){
+				case FMT_U8:{
+					nWrite = fwrite(uBufferIQ,sizeof(u8cmplx),nRead,output_fid);
+				}break;
+				case FMT_I16:{
+				    nWrite = fwrite(iBufferIQ,sizeof(i16cmplx),nRead,output_fid);
+				}break;
+				case FMT_FLOAT:{
+				    nWrite = fwrite(fBufferIQ,sizeof(fcmplx),nRead,output_fid);
+				}break;
+			}
+					
 			if(nWrite<nRead){ fprintf(stderr,"[LOOP RD] Error writing data, quit \n"); break; }
 		
 			FrameCount++;
