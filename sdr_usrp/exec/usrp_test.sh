b29ba43791fe2dedbd352ec014ce43d3d3fdd10e
Bilel Raddadi <bilel.raddadi@irt-saintexupery.com>
2024-02-07 11:37:11 +0100

    clean code

--- sdr_usrp/exec/usrp_test.sh
+++ sdr_usrp/exec/usrp_test.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 
+libdir=~/usrp/lib/loopProg/bin
 killall -9 loopRead.run usrptx.run
 
 SampleRate=1500000
@@ -10,6 +11,4 @@ Gain=5
 #mkfifo capture.iq -m666
 
 
-
-./loopRead.run -i capture_NR_i16.bin -r i16 -w i16 | ./usrptx.run -s $SampleRate -f $CenterFrequency -g $Gain
-
+$libdir/loopRead.run -i capture_NR_i16.bin -r i16 -w i16 | ../bin/usrptx.run -s $SampleRate -f $CenterFrequency -g $Gain
